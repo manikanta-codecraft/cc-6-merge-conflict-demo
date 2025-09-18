7a9fe82997e4419cba7c385b9f58a394950a55cb
Amrutha Acharya <amrutha.acharya@codecraft.co.in>
2025-09-18 16:12:39 +0530

    Amrutha first commit

--- index.ts
+++ index.ts
@@ -1,5 +1,11 @@
 console.log("Hello via Bun!");
 function add(a:number,b:number):number{
-    return a+b;
+    return a+b+10 ;
 }
 add(2,3);
+
+function myname( name : string ): string {
+    return "Hi "+ name;
+}
+
+myname("amrutha");
