4c5b095654c890868e7417b424ac4e499af9bbcb
manikanta <manikanta.d@codecraft.co.in>
2025-09-18 23:02:15 +0530

    solved conflict

--- index.ts
+++ index.ts
@@ -1,11 +1,5 @@
 console.log("Hello via Bun!");
 function add(a:number,b:number):number{
-<<<<<<< HEAD
     return a-b;
-||||||| 69a18f5
-    return a+b;
-=======
-    return a+b+1+1+1;
->>>>>>> main
 }
 add(2,3);
